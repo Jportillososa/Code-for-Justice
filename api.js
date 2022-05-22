1a48a79bd1ad98b73472157937d95a2d57010ffd
brandnn07 <brandcarter4@gmail.com>
2022-05-22 13:50:11 -0400

    adding apis to the homepage

--- api.js
+++ api.js
@@ -1,15 +1,16 @@
-var body = document.body;
 const locationBtn = document.querySelector("#getLocation")
+const container = document.querySelector(".shelterContainer")
 
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
+    } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
 function showPosition(position) {
+    window.initMap = initMap(position);
     fetchAPI(position);
 }
 locationBtn.addEventListener("click", getLocation())
@@ -21,9 +22,9 @@ async function fetchAPI(position) {
     console.log(info);
     info.forEach(element => { //Put .attributes before using the above ^
         var newCard = document.createElement('div');
-        newCard.className = 'card'
+        newCard.className = 'card items'
         var newContainer = document.createElement('div');
-        newContainer.className = 'container'
+        // newContainer.className = 'container'
         var newHeader = document.createElement('h4');
         newHeader.textContent = element.attributes.OWNER_RENTER
         var provider = document.createElement('p');
@@ -48,7 +49,7 @@ async function fetchAPI(position) {
         access.textContent = `How to access shelter: ${element.attributes.HOW_TO_ACCESS}`
         var url = document.createElement('p');
         url.textContent = `Website url (if any): ${element.attributes.WEB_URL}`
-        body.appendChild(newCard);
+        container.appendChild(newCard);
         newCard.append(newContainer);
         newContainer.append(newHeader);
         newContainer.append(provider);
@@ -65,4 +66,74 @@ async function fetchAPI(position) {
     });
     console.log(position);
 }
+
+function initMap(position) {
+    // Create the map.
+    const location = { lat: position.coords.latitude, lng: position.coords.longitude }; //35.00573247895826, -80.84910169025957 the coordinates of RV
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: location,
+        zoom: 17,
+        mapId: "8d193001f940fde3",
+    });
+    // Create the places service.
+    const service = new google.maps.places.PlacesService(map);
+    let getNextPage;
+    const moreButton = document.getElementById("more");
+
+    moreButton.onclick = function () {
+        moreButton.disabled = true;
+        if (getNextPage) {
+            getNextPage();
+        }
+    };
+
+    // Perform a nearby search.
+    service.nearbySearch(
+        { location: location, radius: 3500, type: "real_estate_agency" },
+        (results, status, pagination) => {
+            if (status !== "OK" || !results) return;
+
+            addPlaces(results, map);
+            moreButton.disabled = !pagination || !pagination.hasNextPage;
+            if (pagination && pagination.hasNextPage) {
+                getNextPage = () => {
+                    // Note: nextPage will call the same handler function as the initial call
+                    pagination.nextPage();
+                };
+            }
+        }
+    );
+}
+
+function addPlaces(places, map) {
+    const placesList = document.getElementById("places");
+
+    for (const place of places) {
+        if (place.geometry && place.geometry.location) {
+            const image = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25),
+            };
+
+            new google.maps.Marker({
+                map,
+                icon: image,
+                title: place.name,
+                position: place.geometry.location,
+            });
+
+            const li = document.createElement("li");
+
+            li.textContent = place.name;
+            placesList.appendChild(li);
+            li.addEventListener("click", () => {
+                map.setCenter(place.geometry.location);
+            });
+        }
+    }
+}
+
 // https://data.cityofchicago.org/resource/eep4-c978.json Info for Chicago homelessness crisis
